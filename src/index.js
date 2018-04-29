243d529eb5666db0c7e0c88187100f38510daee5
Joseph Hill <joseph.hill1@outlook.com>
2018-04-29 16:56:39 -0400

    Destructure firebase initializeApp and added firebase deploy script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
 import { initializeApp } from 'firebase';
 import App from './components/App.jsx'
 
